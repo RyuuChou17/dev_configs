3ba6f86cc7bb69d798ec454d8312f39ef60b320f
RyuuChou17 <129361120+RyuuChou17@users.noreply.github.com>
2025-08-01 23:30:27 +0900

    feat: enable latex snippets

--- lua/main.lua
+++ lua/main.lua
@@ -150,20 +150,45 @@ lspconfig.texlab.setup({
 
 
 local cmp = require("cmp")
+local luasnip = require("luasnip")
 cmp.setup({
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
+    },
     mapping = cmp.mapping.preset.insert({
-        ['<Tab>'] = cmp.mapping.select_next_item(),
-        ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
         ['<CR>'] = cmp.mapping.confirm({ select = true }),
     }),
     sources = {
         { name = "nvim_lsp" },
+        { name = "luasnip" },
         { name = "buffer" },
         { name = "path" },
         { name = "vimtex" },
     },
 })
 
+require("luasnip-latex-snippets").setup()
+require("luasnip").config.setup { enable_autosnippets = true }
 
 local null_ls = require("null-ls")
 null_ls.setup({
